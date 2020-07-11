69da23a3705810a7190b3fd74bb8e752a465f215
Katherine Jones <kejone1414@gmail.com>
2020-07-10 23:28:12 -0400

    fix: modified error in cached file name

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,8 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/favicon.ico",
     "/manifest.webmanifest",
-    "/style.css",
+    "/styles.css",
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
